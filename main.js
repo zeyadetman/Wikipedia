6458b54f957624381a2961bf3d89abdf485a232a
zeyadetman <zeyadetman@gmail.com>
2018-04-01 05:18:51 +0200

    Code refactoring

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 function searcher() {
     $(".data").html(" ");
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=12&prop=pageimages%7Cextracts&pilimit=max&exintro=&explaintext=&exsentences=1&exlimit=max&gsrsearch=${$(".searchquery ").val()}`,
-        "method": "GET",
-        "headers": {
-            "cache-control": "no-cache",
-            "postman-token": "ba2e8bb7-fd67-4767-9d9d-959783f71f08"
-        }
-    }
-
-    $.ajax(settings).done(function(response) {
+    let link = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=12&prop=pageimages%7Cextracts&pilimit=max&exintro=&explaintext=&exsentences=1&exlimit=max&gsrsearch=${$(".searchquery ").val()}&origin=*`;
+fetch(link)
+  .then(res => res.json())
+    .then((response)=>{
         //console.log(response);
-        var res = response['query']['pages'];
+        let res = response['query']['pages'];
+        //console.log(res);
         var w = Object.keys(res);
         //console.log(w);
         for (var i = 0; i < 12; i++) {
-            //  console.log(res[w[i]]);
+            //console.log(res[w[i]]);
             var src = "null";
             if (res[w[i]]['thumbnail'] == undefined)
                 src = null;
@@ -27,7 +20,6 @@ function searcher() {
             $(".data").append(`<div class="col-md-3 item"> <img class="img-responsive img-rounded" src="${src}">
             <a href="https://en.wikipedia.org/?curid=${res[w[i]]['pageid']}" target="_blank">${res[w[i]]['title']}</a>
             <p class="description">${res[w[i]]['extract']}</p></div>`);
-
         }
-    });
+}).catch(()=>console.log('err'));
 }
